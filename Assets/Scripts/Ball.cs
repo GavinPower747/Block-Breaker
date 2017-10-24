0bac2cd4f5ef2f53958f22c17f95cdd1b7d84090
GavinPower747 <gavinpower747@hotmail.com>
2017-10-24 22:50:33 +0100

    Greatly simplified movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,15 +1,39 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Player;
+using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float GradualFall = 0.03f;
+
+    private PlayerController _player;
+    private Rigidbody2D _rigidBody;
+
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, _rigidBody.velocity.y - GradualFall); 
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         var brickComponent = collision.gameObject.GetComponent<Brick>();
 
         if (brickComponent != null)
             brickComponent.Hit();
-        
-        if(collision.gameObject.tag == "Floor")
-            GameManager.Instance.GameOver();        
+
+        if (collision.gameObject.tag == "Floor")
+        {
+            var gameOver = _player.Die();
+
+            if (!gameOver)
+                Destroy(gameObject);
+            else
+                _rigidBody.velocity = Vector2.zero;
+        }
     }
 }
